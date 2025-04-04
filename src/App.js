0558b2b83b6a6e23a8b0bc7de87fbc78e45f6e9c
RubensCarvalhoRocha <rocharubens4@gmail.com>
2025-04-03 22:57:14 -0300

    app set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GraficoBeneficiarios from "./GraficoBeneficiarios";
+
+function App() {
+  return (
+    <div>
+      <GraficoBeneficiarios />
+    </div>
+  );
+}
+
+export default App;
