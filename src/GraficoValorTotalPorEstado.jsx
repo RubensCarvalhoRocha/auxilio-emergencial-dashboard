92f49d7cb0adcb8d899905ad9e9638f11e8c4609
RubensCarvalhoRocha <rocharubens4@gmail.com>
2025-04-04 00:33:22 -0300

    feat: Alterando o tipo de grafico do dashboard valro total por estado

--- src/GraficoValorTotalPorEstado.jsx
+++ src/GraficoValorTotalPorEstado.jsx
@@ -10,7 +10,6 @@ const GraficoValorTotalPorEstado = () => {
   useEffect(() => {
     axios.get("http://127.0.0.1:5000/valores-totais/")
       .then(response => {
-        // Remove o total nacional (BR) e ordena do maior para o menor
         const filtradoOrdenado = response.data
           .filter(item => item.uf !== "BR")
           .map(item => ({
@@ -27,13 +26,20 @@ const GraficoValorTotalPorEstado = () => {
   }, []);
 
   return (
-    <div style={{ width: '100%', height: 600 }}>
-      <h2>Valor Total Pago por Estado</h2>
+    <div style={{ width: '100%', height: 1000 }}>
+      <h2>Valor Total Pago por Estado (Barras Horizontais)</h2>
       {dados.length > 0 && (
         <ResponsiveContainer>
-          <BarChart data={dados} margin={{ top: 20, right: 30, left: 20, bottom: 60 }}>
-            <XAxis dataKey="uf" angle={-45} textAnchor="end" interval={0} />
-            <YAxis tickFormatter={(value) => `R$ ${(value / 1_000_000_000).toFixed(1)}Bi`} />
+          <BarChart
+            layout="vertical"
+            data={dados}
+            margin={{ top: 20, right: 30, left: 100, bottom: 20 }}
+          >
+            <XAxis
+              type="number"
+              tickFormatter={(value) => `R$ ${(value / 1_000_000_000).toFixed(1)}Bi`}
+            />
+            <YAxis dataKey="uf" type="category" width={80} />
             <Tooltip formatter={(value) => `R$ ${value.toLocaleString('pt-BR')}`} />
             <Legend />
             <Bar dataKey="valor_total" fill="#0088FE" name="Valor Total Pago" />
